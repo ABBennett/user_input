7128418efb8a38e901ec51239d49df776535d590
AlexBennett <alexanderbondbennett@gmail.com>
2016-02-20 12:01:47 -0500

    accepted user input and added it to a string

---
+++ user_input/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace user_input
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.Write ("What is your name?");
+
+			string name = Console.ReadLine ();
+			Console.Write ("Hello, {0}! It's nice to finally meet you.", name);
+
+			Console.WriteLine ("Welcome to this computer");
+		}
+	}
+}
